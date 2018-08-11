e950159c2e9075fee4c109a4297b35db00960913
xinjie-just <18227752005@qq.com>
2018-08-11 22:01:26 +0800

    1.添加思源黑体-常规字体；2.添加iconfont图标；3.完成首页底部

---
+++ scripts/meipo.js
@@ -0,0 +1,25 @@
+/**
+ * Created by lixinjie on 2018/8/11.
+ */
+/*回到顶部*/
+$(window).scroll(function() {
+    if($(window).scrollTop() >= 200) {
+        $("#toTop").fadeIn(); /* 当滑动到不小于 200px 时，回到顶部图标显示 */
+    }else {
+        $("#toTop").fadeOut(); /* 当滑动到小于(页面被卷去的高度)200px 时，回到顶部图标隐藏 */
+    }
+});
+$("#toTop").click(function () {
+    $("html, body").animate({scrollTop: 0}, 600); /* 持续时间为 600ms */
+    return false;
+});
+
+
+// 微信二维码的显示与影藏
+$("#wechatIcon").hover(
+    function(){
+        $("#qrcode").slideDown(400);
+    },function() {
+        $("#qrcode").slideUp(400);
+    }
+);
