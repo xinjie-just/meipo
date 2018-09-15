7f9390b9ae35498743191885df343e8aab6ab21d
xinjie-just <18227752005@qq.com>
2018-09-15 22:25:27 +0800

    新闻列表页面完成

--- scripts/meipo.js
+++ scripts/meipo.js
@@ -4,9 +4,9 @@
 /*回到顶部*/
 $(window).scroll(function() {
     if($(window).scrollTop() >= 200) {
-        $("#toTop").fadeIn(); /* 当滑动到不小于 200px 时，回到顶部图标显示 */
+        $(".js-to-top").fadeIn(); /* 当滑动到不小于 200px 时，回到顶部图标显示 */
     }else {
-        $("#toTop").fadeOut(); /* 当滑动到小于(页面被卷去的高度)200px 时，回到顶部图标隐藏 */
+        $(".js-to-top").fadeOut(); /* 当滑动到小于(页面被卷去的高度)200px 时，回到顶部图标隐藏 */
     }
 
     // 头部
@@ -16,7 +16,7 @@ $(window).scroll(function() {
         $(".js-heading-wrapper").removeClass("scroll");
     }
 });
-$("#toTop").click(function () {
+$(".js-to-top").click(function () {
     $("html, body").animate({scrollTop: 0}, 600); /* 持续时间为 600ms */
     return false;
 });
@@ -25,8 +25,27 @@ $("#toTop").click(function () {
 // 微信二维码的显示与影藏
 $("#wechatIcon").hover(
     function(){
-        $("#qrcode").slideDown(400);
+        $(".js-qrcode").slideDown(400);
     },function() {
-        $("#qrcode").slideUp(400);
+        $(".js-qrcode").slideUp(400);
     }
 );
+// 重新加载页面时
+$(function () {
+    matchUrl();
+});
+$(".js-tab").find("a").click(function () {
+    $(this).addClass("active");
+    $(this).siblings("a").removeClass("active");
+});
+// 当前页面地址与路由匹配情况
+function matchUrl() {
+    var link =  $(".js-tab").find("a");
+    var locationUrl = location.href;
+    for (var i = 0, len = link.length; i < len; i++) {
+        if (locationUrl.indexOf($(link[i]).attr("href")) !== -1) {
+            $(link[i]).addClass("active");
+            $(link[i]).siblings("a").removeClass("active");
+        }
+    }
+}
