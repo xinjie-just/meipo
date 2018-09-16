97f5c56467a68fbc115eee6ba258ede66237ff91
xinjie-just <18227752005@qq.com>
2018-09-16 15:31:16 +0800

    关于美珀页面完成

--- scripts/meipo.js
+++ scripts/meipo.js
@@ -1,39 +1,51 @@
 /**
  * Created by lixinjie on 2018/8/11.
  */
+// 重新加载页面时
+$(function () {
+    matchUrl();
+    changeHeader();
+    toTopDisplay();
+});
+
 /*回到顶部*/
 $(window).scroll(function() {
-    if($(window).scrollTop() >= 200) {
-        $(".js-to-top").fadeIn(); /* 当滑动到不小于 200px 时，回到顶部图标显示 */
-    }else {
-        $(".js-to-top").fadeOut(); /* 当滑动到小于(页面被卷去的高度)200px 时，回到顶部图标隐藏 */
-    }
-
-    // 头部
-    if($(window).scrollTop() > 0) {
-        $(".js-heading-wrapper").addClass("scroll");
-    } else {
-        $(".js-heading-wrapper").removeClass("scroll");
-    }
+    toTopDisplay();
+    changeHeader();
 });
 $(".js-to-top").click(function () {
     $("html, body").animate({scrollTop: 0}, 600); /* 持续时间为 600ms */
     return false;
 });
 
+// 回到顶部的显示或隐藏
+function toTopDisplay() {
+    if($(window).scrollTop() >= 200) {
+        $(".js-to-top").fadeIn(); /* 当滑动到不小于 200px 时，回到顶部图标显示 */
+    }else {
+        $(".js-to-top").fadeOut(); /* 当滑动到小于(页面被卷去的高度)200px 时，回到顶部图标隐藏 */
+    }
+}
+
+// 改变头部样式
+function changeHeader() {
+    // 头部
+    if($(window).scrollTop() > 0) {
+        $(".js-heading-wrapper").addClass("scroll");
+    } else {
+        $(".js-heading-wrapper").removeClass("scroll");
+    }
+}
 
 // 微信二维码的显示与影藏
-$("#wechatIcon").hover(
+$(".js-wechat-icon").hover(
     function(){
         $(".js-qrcode").slideDown(400);
     },function() {
         $(".js-qrcode").slideUp(400);
     }
 );
-// 重新加载页面时
-$(function () {
-    matchUrl();
-});
+
 $(".js-tab").find("a").click(function () {
     $(this).addClass("active");
     $(this).siblings("a").removeClass("active");
